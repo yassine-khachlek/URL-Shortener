21d3998f95c98841ac059836daa861bddc85fc1a
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-18 04:16:42 +0100

    Added user<->url relations

--- app/Url.php
+++ app/Url.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Url extends Model
 {
-    //
+    /**
+     * Get the user that owns the url.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the urls for the user.
+     */
+    public function urls()
+    {
+        return $this->hasMany('App\Url');
+    }
 }
