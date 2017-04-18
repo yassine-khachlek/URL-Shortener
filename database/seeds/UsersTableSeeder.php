14d2cfde6c5ada70ca17baf039c0804cc7ae9722
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-18 04:07:10 +0100

    Added default users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    factory(App\User::class, 1)->create([
+	    	'name' => 'Yassine Khachlek',
+	    	'email' => 'yassine.khachlek@gmail.com',
+	    	'password' => bcrypt('secret')
+	    ]);
+    }
+}
