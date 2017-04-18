40eae8835aefdc45a35269d827e0d8342092f547
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-18 07:33:08 +0100

    Added countries

---
+++ database/migrations/2014_10_12_000000_create_countries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create((new App\Country)->getTable(), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code', 2);
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists((new App\Country)->getTable());
+    }
+}
---
+++ database/seeds/CountriesTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Country;
+
+class CountriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $header = [];
+        $rows   = [];
+        $count  = -1;
+        
+        if (($handle = fopen(base_path('database/seeds/data/countries.csv'), 'r')) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                $count++;
+                if( $count < 1 )
+                {
+                    for ($c=0; $c < $num; $c++) {
+                        $header[$c] = $data[$c];
+                    }
+                }else{
+                    $rows[$count] = [];
+                    for ($c=0; $c < $num; $c++) {
+                        if ($data[$c]==='null') {
+                            $data[$c] = null;
+                        }
+                        $rows[$count][$header[$c]] = $data[$c];
+                    }
+                }
+            }
+            fclose($handle);
+        }
+
+        foreach ($rows as $row) {
+            Country::firstOrCreate(['code' => strtolower($row['code'])], [
+            	'code' => strtolower($row['code']),
+            	'name' => $row['name'],
+            ]);
+        }
+    }
+}
