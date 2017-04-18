773b1ac50769a753664e8a52d5c6fc824b1d2aa2
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-18 10:03:41 +0100

    Added Geo lite migration

---
+++ database/migrations/2017_04_18_074917_create_geo_lites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGeoLitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create((new App\GeoLite)->getTable(), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('start', 45);
+            $table->string('end', 45);
+            $table->string('country_code', 2);
+            $table->timestamps();
+
+            $table->index(['start', 'end']);
+
+            $table->foreign('country_code')->references('code')->on((new App\Country)->getTable());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists((new App\GeoLite)->getTable());
+    }
+}
