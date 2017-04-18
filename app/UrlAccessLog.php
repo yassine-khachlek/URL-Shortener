e49004c8b5501162fb7414eae47b83d58387d3f1
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-18 08:35:17 +0100

    Added access log relations

--- app/UrlAccessLog.php
+++ app/UrlAccessLog.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class UrlAccessLog extends Model
 {
-    //
+    /**
+     * Get the user that owns the log.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the url that owns the log.
+     */
+    public function url()
+    {
+        return $this->belongsTo('App\Url');
+    }
+
+    /**
+     * Get the user agent that owns the log.
+     */
+    public function userAgent()
+    {
+        return $this->belongsTo('App\UserAgent');
+    }
+
+    /**
+     * Get the country that owns the log.
+     */
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
 }
