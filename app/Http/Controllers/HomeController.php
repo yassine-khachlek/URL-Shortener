4dc1ef41369e0c6ae02fa42899a3bc061fb2ab3b
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-18 11:47:22 +0100

    Added users and links count in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+use App\Url;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +26,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return (new UrlsController)->index();
+        $users_count = User::count();
+        $urls_count = Url::count();
+
+        return view('home', compact('users_count', 'urls_count'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="panel panel-default">
-    <div class="panel-heading">Dashboard</div>
-
-    <div class="panel-body">
-
-        
-
+<div class="panel-body">
+	<div class="row">
+        <div class="col-lg-6 col-md-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-users fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge">{{ $users_count }}</div>
+                            <div>Users!</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ Route::has('users.index') ? route('users.index') : '#' }}">
+                    <div class="panel-footer">
+                        <span class="pull-left">View Details</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-link fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge">{{ $urls_count }}</div>
+                            <div>Urls!</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ Route::has('urls.index') ? route('urls.index') : '#' }}">
+                    <div class="panel-footer">
+                        <span class="pull-left">View Details</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
 @append
