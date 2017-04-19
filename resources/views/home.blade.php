f369129315d6e1a3be1100e022bad3c4065988c9
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-19 12:09:28 +0100

    working in user home page..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,53 @@
 
 @section('content')
 <div class="row">
+    <div class="col-lg-12 col-md-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-link fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge">{{ Auth::user()->urls()->count() }} / {{ Config::get('url.limit_per_user') }}</div>
+                        <div>Urls!</div>
+                    </div>
+                </div>
+            </div>
+            <a href="{{ Route::has('urls.index') ? route('urls.index') : '#' }}">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-12 col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <i class="fa fa-line-chart" aria-hidden="true"></i>
+            Urls views
+          </div>
+          <div class="panel-body">
+            @include('commons.widgets.charts.chartjs.line', [
+                'id'    =>  'url_views',
+                'label' =>  'URL views',
+                'labels'=>  Auth::user()->urls()->get()->map(function($item){
+                                return $item->url_short;
+                            }),
+                'data'  =>  Auth::user()->urls()->get()->map(function($item){
+                                return $item->views_count;
+                            }),
+            ])
+          </div>
+        </div>
+    </div>
+</div>
+
+@if(Auth::user()->is_admin)
+<div class="row">
     <div class="col-lg-6 col-md-12">
         <div class="panel panel-primary">
             <div class="panel-heading">
@@ -86,6 +133,7 @@
         </div>
     </div>
 </div>
+@endif
 @append
 
 @section('scripts')
